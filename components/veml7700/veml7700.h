d3f5487dedaa58df241b0b8d4cd881880befc954
pipacsba <44190358+pipacsba@users.noreply.github.com>
2023-06-13 13:37:07 +0200

    Adopted to VEML7700

--- components/veml7700/veml7700.h
+++ components/veml7700/veml7700.h
@@ -5,29 +5,34 @@
 #include "esphome/components/i2c/i2c.h"
 
 namespace esphome {
-namespace tsl2561 {
+namespace veml7700 {
 
 /** Enum listing all conversion/integration time settings for the TSL2561
  *
  * Higher values mean more accurate results, but will take more energy/more time.
  */
-enum TSL2561IntegrationTime {
-  TSL2561_INTEGRATION_14MS = 0b00,
-  TSL2561_INTEGRATION_101MS = 0b01,
-  TSL2561_INTEGRATION_402MS = 0b10,
+enum VEML7700IntegrationTime {
+  VEML7700_INTEGRATION_25MS  = 0x300,
+  VEML7700_INTEGRATION_50MS  = 0x200,
+  VEML7700_INTEGRATION_100MS = 0x00,
+  VEML7700_INTEGRATION_200MS = 0x40,
+  VEML7700_INTEGRATION_400MS = 0x80,
+  VEML7700_INTEGRATION_800MS = 0xC0,
 };
 
 /** Enum listing all gain settings for the TSL2561.
  *
  * Higher values are better for low light situations, but can increase noise.
  */
-enum TSL2561Gain {
-  TSL2561_GAIN_1X = 0,
-  TSL2561_GAIN_16X = 1,
+enum VEML7700Gain {
+  VEML7700_GAIN_1P4X = 0x1800,
+  VEML7700_GAIN_1P8X = 0x1000,
+  VEML7700_GAIN_2X = 0x800,
+  VEML7700_GAIN_1X = 0x0,
 };
 
 /// This class includes support for the TSL2561 i2c ambient light sensor.
-class TSL2561Sensor : public sensor::Sensor, public PollingComponent, public i2c::I2CDevice {
+class VEML7700Sensor : public sensor::Sensor, public PollingComponent, public i2c::I2CDevice {
  public:
   /** Set the time that sensor values should be accumulated for.
    *
@@ -41,7 +46,7 @@ class TSL2561Sensor : public sensor::Sensor, public PollingComponent, public i2c
    *
    * @param integration_time The new integration time.
    */
-  void set_integration_time(TSL2561IntegrationTime integration_time);
+  void set_integration_time(VEML7700IntegrationTime integration_time);
 
   /** Set the internal gain of the sensor. Can be useful for low-light conditions
    *
@@ -52,7 +57,7 @@ class TSL2561Sensor : public sensor::Sensor, public PollingComponent, public i2c
    *
    * @param gain The new gain.
    */
-  void set_gain(TSL2561Gain gain);
+  void set_gain(VEML7700Gain gain);
 
   /** The "CS" package of this sensor has a slightly different formula for
    * converting the raw values. Use this setting to indicate that this is a CS
@@ -60,7 +65,6 @@ class TSL2561Sensor : public sensor::Sensor, public PollingComponent, public i2c
    *
    * @param package_cs Is this a CS package.
    */
-  void set_is_cs_package(bool package_cs);
 
   // ========== INTERNAL METHODS ==========
   // (In most use cases you won't need these)
@@ -69,19 +73,18 @@ class TSL2561Sensor : public sensor::Sensor, public PollingComponent, public i2c
   void update() override;
   float get_setup_priority() const override;
 
-  bool tsl2561_read_byte(uint8_t a_register, uint8_t *value);
-  bool tsl2561_read_uint(uint8_t a_register, uint16_t *value);
-  bool tsl2561_write_byte(uint8_t a_register, uint8_t value);
+  bool veml7700_read_byte(uint8_t a_register, uint8_t *value);
+  bool veml7700_read_uint(uint8_t a_register, uint16_t *value);
+  bool veml7700_write_byte(uint8_t a_register, uint8_t value);
 
  protected:
   float get_integration_time_ms_();
   void read_data_();
   float calculate_lx_(uint16_t ch0, uint16_t ch1);
 
-  TSL2561IntegrationTime integration_time_{TSL2561_INTEGRATION_402MS};
-  TSL2561Gain gain_{TSL2561_GAIN_1X};
-  bool package_cs_{false};
+  VEML7700IntegrationTime integration_time_{TSL2561_INTEGRATION_402MS};
+  VEML7700Gain gain_{TSL2561_GAIN_1X};
 };
 
-}  // namespace tsl2561
+}  // namespace veml7700
 }  // namespace esphome
