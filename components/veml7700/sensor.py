ee3e63eec4b3ba8982d97a314205531e3ecba2fa
pipacsba <44190358+pipacsba@users.noreply.github.com>
2023-06-13 13:29:32 +0200

    Adjusted to VEML7700

--- components/veml7700/sensor.py
+++ components/veml7700/sensor.py
@@ -11,51 +11,52 @@ from esphome.const import (
 
 DEPENDENCIES = ["i2c"]
 
-tsl2561_ns = cg.esphome_ns.namespace("tsl2561")
-TSL2561IntegrationTime = tsl2561_ns.enum("TSL2561IntegrationTime")
+veml7700_ns = cg.esphome_ns.namespace("veml7700")
+VEML7700IntegrationTime = veml7700_ns.enum("VEML7700IntegrationTime")
 INTEGRATION_TIMES = {
-    14: TSL2561IntegrationTime.TSL2561_INTEGRATION_14MS,
-    101: TSL2561IntegrationTime.TSL2561_INTEGRATION_101MS,
-    402: TSL2561IntegrationTime.TSL2561_INTEGRATION_402MS,
+    25: VEML7700IntegrationTime.VEML7700_INTEGRATION_25MS,
+    50: VEML7700IntegrationTime.VEML7700_INTEGRATION_50MS,
+    100: VEML7700IntegrationTime.VEML7700_INTEGRATION_100MS,
+    200: VEML7700IntegrationTime.VEML7700_INTEGRATION_200MS,
+    400: VEML7700IntegrationTime.VEML7700_INTEGRATION_400MS,
+    800: VEML7700IntegrationTime.VEML7700_INTEGRATION_800MS,
 }
 
-TSL2561Gain = tsl2561_ns.enum("TSL2561Gain")
+VEML7700Gain = veml7700_ns.enum("VEML7700Gain")
 GAINS = {
-    "1X": TSL2561Gain.TSL2561_GAIN_1X,
-    "16X": TSL2561Gain.TSL2561_GAIN_16X,
+    "1X": VEML7700Gain.VEML7700_ALS_GAIN_1X,
+    "1.4X": VEML7700Gain.VEML7700_ALS_GAIN_1P4X,
+    "1.8X": VEML7700Gain.VEML7700_ALS_GAIN_1P8X,
+    "2X": VEML7700Gain.VEML7700_ALS_GAIN_2X,
 }
-
-CONF_IS_CS_PACKAGE = "is_cs_package"
-
 
 def validate_integration_time(value):
     value = cv.positive_time_period_milliseconds(value).total_milliseconds
     return cv.enum(INTEGRATION_TIMES, int=True)(value)
 
 
-TSL2561Sensor = tsl2561_ns.class_(
-    "TSL2561Sensor", sensor.Sensor, cg.PollingComponent, i2c.I2CDevice
+VEML7700Sensor = veml7700_ns.class_(
+    "VEML7700Sensor", sensor.Sensor, cg.PollingComponent, i2c.I2CDevice
 )
 
 CONFIG_SCHEMA = (
     sensor.sensor_schema(
-        TSL2561Sensor,
+        VEML7700Sensor,
         unit_of_measurement=UNIT_LUX,
-        accuracy_decimals=1,
+        accuracy_decimals=2,
         device_class=DEVICE_CLASS_ILLUMINANCE,
         state_class=STATE_CLASS_MEASUREMENT,
     )
     .extend(
         {
             cv.Optional(
-                CONF_INTEGRATION_TIME, default="402ms"
+                CONF_INTEGRATION_TIME, default="100ms"
             ): validate_integration_time,
-            cv.Optional(CONF_GAIN, default="1X"): cv.enum(GAINS, upper=True),
-            cv.Optional(CONF_IS_CS_PACKAGE, default=False): cv.boolean,
+            cv.Optional(CONF_GAIN, default="2X"): cv.enum(GAINS, upper=True),
         }
     )
     .extend(cv.polling_component_schema("60s"))
-    .extend(i2c.i2c_device_schema(0x39))
+    .extend(i2c.i2c_device_schema(0x10))
 )
 
 
@@ -66,4 +67,4 @@ async def to_code(config):
 
     cg.add(var.set_integration_time(config[CONF_INTEGRATION_TIME]))
     cg.add(var.set_gain(config[CONF_GAIN]))
-    cg.add(var.set_is_cs_package(config[CONF_IS_CS_PACKAGE]))
+    
