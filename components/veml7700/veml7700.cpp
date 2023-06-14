6637523bbc2eac3fe05f1154bd71097a017ef10c
pipacsba <44190358+pipacsba@users.noreply.github.com>
2023-06-14 17:13:59 +0200

    Update veml7700.cpp

--- components/veml7700/veml7700.cpp
+++ components/veml7700/veml7700.cpp
@@ -37,124 +37,130 @@ void VEML7700Sensor::setup() {
   uint16_t gain = this->gain_;
   uint16_t psm = this->psm_
     
-  this->veml7700_write_uint(CONFIGURATION_REGISTER, ALS_POWERON | integration_time | gain);
-  ESP_LOGCONFIG(TAG, "Power on, integration tiem, and gain are set");
+  if (!this->veml7700_write_uint(CONFIGURATION_REGISTER, ALS_POWERON | integration_time | gain)) 
+  {
+     this->mark_failed();
+     return;   
+  }
+  else
+  {
+    ESP_LOGCONFIG(TAG, "Power on, integration tiem, and gain are set");
+  }
   
-  this->veml7700_write_uint(POWER_SAVING_REGISTER, PSM_EN | psm);
-  ESP_LOGCONFIG(TAG, "Power save registers are set");
+  if (!this->veml7700_write_uint(POWER_SAVING_REGISTER, PSM_EN | psm))
+  {
+     this->mark_failed();
+     return;   
+  }
+  else
+  {
+    ESP_LOGCONFIG(TAG, "Power save registers are set");
+  }
 }
   
-void TSL2561Sensor::dump_config() {
-  LOG_SENSOR("", "TSL2561", this);
+void VEML7700Sensor::dump_config() {
+  LOG_SENSOR("", "VEML7700", this);
   LOG_I2C_DEVICE(this);
 
   if (this->is_failed()) {
-    ESP_LOGE(TAG, "Communication with TSL2561 failed!");
+    ESP_LOGE(TAG, "Communication with VEML7700 failed!");
   }
 
-  int gain = this->gain_ == TSL2561_GAIN_1X ? 1 : 16;
-  ESP_LOGCONFIG(TAG, "  Gain: %dx", gain);
+  int gain = this->gain_;
+  ESP_LOGCONFIG(TAG, "  Gain: %.3f x", this->get_gain());
   ESP_LOGCONFIG(TAG, "  Integration Time: %.1f ms", this->get_integration_time_ms_());
 
   LOG_UPDATE_INTERVAL(this);
 }
-void TSL2561Sensor::update() {
-  // Power on
-  if (!this->tsl2561_write_byte(TSL2561_REGISTER_CONTROL, 0b00000011)) {
-    this->status_set_warning();
-    return;
-  }
-
-  // Make sure the data is there when we will read it.
-  auto timeout = static_cast<uint32_t>(this->get_integration_time_ms_() + 20);
-
-  this->set_timeout("illuminance", timeout, [this]() { this->read_data_(); });
+void VEML7700Sensor::update() {
+  this->read_data_();
 }
 
-float TSL2561Sensor::calculate_lx_(uint16_t ch0, uint16_t ch1) {
-  if ((ch0 == 0xFFFF) || (ch1 == 0xFFFF)) {
-    ESP_LOGW(TAG, "TSL2561 sensor is saturated.");
-    return NAN;
+float VEML7700Sensor::calculate_lx_(uint16_t als) {
+  float magic_number = 0.0288;
+  switch (this->integration_time_)
+  {
+    case VEML7700_INTEGRATION_200MS:
+      magic_number = 0.0144;
+    case VEML7700_INTEGRATION_400MS:
+      magic_number = 0.0072;
+    case VEML7700_INTEGRATION_800MS:
+      magic_number = 0.0036;
+    case VEML7700_INTEGRATION_50MS:
+      magic_number = 0.0576;
+    case VEML7700_INTEGRATION_25MS:
+      magic_number = 0.1152;
+    default:
+      magic_number = 0.0288;
+  } 
+  float a_gain = 1;
+  switch (this->gain_)
+  {
+    case VEML7700_GAIN_1X :
+      a_gain = 0.5;
+    case VEML7700_GAIN_0p25X :
+      a_gain = 8;
+    case VEML7700_GAIN_0p125X :
+      a_gain = 16;
+    default:
+      a_gain = 1;
   }
-
-  float d0 = ch0, d1 = ch1;
-  float ratio = d1 / d0;
-
-  float ms = this->get_integration_time_ms_();
-  d0 *= (402.0f / ms);
-  d1 *= (402.0f / ms);
-
-  if (this->gain_ == TSL2561_GAIN_1X) {
-    d0 *= 16;
-    d1 *= 16;
-  }
-
-  if (this->package_cs_) {
-    if (ratio < 0.52f) {
-      return 0.0315f * d0 - 0.0593f * d0 * powf(ratio, 1.4f);
-    } else if (ratio < 0.65f) {
-      return 0.0229f * d0 - 0.0291f * d1;
-    } else if (ratio < 0.80f) {
-      return 0.0157f * d0 - 0.0153f * d1;
-    } else if (ratio < 1.30f) {
-      return 0.00338f * d0 - 0.00260f * d1;
-    }
-
-    return 0.0f;
-  } else {
-    if (ratio < 0.5f) {
-      return 0.0304f * d0 - 0.062f * d0 * powf(ratio, 1.4f);
-    } else if (ratio < 0.61f) {
-      return 0.0224f * d0 - 0.031f * d1;
-    } else if (ratio < 0.80f) {
-      return 0.0128f * d0 - 0.0153f * d1;
-    } else if (ratio < 1.30f) {
-      return 0.00146f * d0 - 0.00112f * d1;
-    }
-    return 0.0f;
-  }
+  return als * magic_number * a gain;
 }
-void TSL2561Sensor::read_data_() {
-  uint16_t data1, data0;
-  if (!this->tsl2561_read_uint(TSL2561_WORD_BIT | TSL2561_REGISTER_DATA_1, &data1)) {
+  
+  
+void VEML7700Sensor::read_data_() {
+  uint16_t als;
+  if (!this->veml7700_read_uint(ALS_REGISTER, &als)) {
     this->status_set_warning();
     return;
   }
-  if (!this->tsl2561_read_uint(TSL2561_WORD_BIT | TSL2561_REGISTER_DATA_0, &data0)) {
-    this->status_set_warning();
-    return;
-  }
-  // Power off
-  if (!this->tsl2561_write_byte(TSL2561_REGISTER_CONTROL, 0b00000000)) {
-    this->status_set_warning();
-    return;
-  }
-
-  float lx = this->calculate_lx_(data0, data1);
+  float lx = this->calculate_lx_(als);
   ESP_LOGD(TAG, "Got illuminance=%.1flx", lx);
   this->publish_state(lx);
   this->status_clear_warning();
 }
-float TSL2561Sensor::get_integration_time_ms_() {
+  
+float VEML7700Sensor::get_integration_time_ms_() {
   switch (this->integration_time_) {
-    case TSL2561_INTEGRATION_14MS:
-      return 13.7f;
-    case TSL2561_INTEGRATION_101MS:
+    case VEML7700_INTEGRATION_200MS:
+      return 200.0f;
+    case VEML7700_INTEGRATION_400MS:
+      return 400.0f;
+    case VEML7700_INTEGRATION_800MS:
+      return 800.0f;
+    case VEML7700_INTEGRATION_50MS:
+      return 50.0f;
+    case VEML7700_INTEGRATION_25MS:
+      return 25.0f;
+    default:
       return 100.0f;
-    case TSL2561_INTEGRATION_402MS:
-      return 402.0f;
   }
   return 0.0f;
 }
-void TSL2561Sensor::set_integration_time(TSL2561IntegrationTime integration_time) {
+  
+float VEML7700Sensor::get_gain_() {
+  switch (this->gain_) {
+    case VEML7700_GAIN_1X :
+      return 1.0f;
+    case VEML7700_GAIN_0p25X :
+      return 0.25f;
+    case VEML7700_GAIN_0p125X :
+      return 0.125f;
+    default:
+      return 2f;
+  }
+  return 0.0f;
+}
+  
+void VEML7700Sensor::set_integration_time(VEML7700IntegrationTime integration_time) {
   this->integration_time_ = integration_time;
 }
-void TSL2561Sensor::set_gain(TSL2561Gain gain) { this->gain_ = gain; }
-void TSL2561Sensor::set_is_cs_package(bool package_cs) { this->package_cs_ = package_cs; }
+void VEML7700Sensor::set_gain(VEML7700Gain gain) { this->gain_ = gain; }
+void VEML7700Sensor::set_pms(VEML7700PMS pms) { this->pms_ = pms; }
+  
 float TSL2561Sensor::get_setup_priority() const { return setup_priority::DATA; }
-bool TSL2561Sensor::tsl2561_write_byte(uint8_t a_register, uint8_t value) {
-  return this->write_byte(a_register | TSL2561_COMMAND_BIT, value);
-}
+
 bool TSL2561Sensor::tsl2561_read_uint(uint8_t a_register, uint16_t *value) {
   uint8_t data[2];
   if (!this->read_bytes(a_register | TSL2561_COMMAND_BIT, data, 2))
@@ -163,9 +169,6 @@ bool TSL2561Sensor::tsl2561_read_uint(uint8_t a_register, uint16_t *value) {
   const uint16_t lo = data[0];
   *value = (hi << 8) | lo;
   return true;
-}
-bool TSL2561Sensor::tsl2561_read_byte(uint8_t a_register, uint8_t *value) {
-  return this->read_byte(a_register | TSL2561_COMMAND_BIT, value);
 }
 
 }  // namespace tsl2561
