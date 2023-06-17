d65dc59555c5c0b76def2322560ae27f2225b0ea
pipacsba <44190358+pipacsba@users.noreply.github.com>
2023-06-17 11:13:06 +0200

    Update veml7700.cpp

--- components/veml7700/veml7700.cpp
+++ components/veml7700/veml7700.cpp
@@ -44,7 +44,7 @@ void VEML7700Sensor::setup() {
   }
   else
   {
-    ESP_LOGCONFIG(TAG, "Power on, integration time, and gain are set %u", ALS_POWERON | integration_time | gain));
+    ESP_LOGCONFIG(TAG, "Power on, integration time, and gain are set %u", ALS_POWERON | integration_time | gain);
   }
   
   if (!this->veml7700_write_uint(POWER_SAVING_REGISTER, PSM_EN | psm))
