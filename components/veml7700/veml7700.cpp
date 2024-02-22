b7267794c611cd5be00fdbe1dbc386dc11788189
pipacsba <44190358+pipacsba@users.noreply.github.com>
2024-02-22 19:00:23 +0100

    Update based on VEML3235 component

--- components/veml7700/veml7700.cpp
+++ components/veml7700/veml7700.cpp
@@ -1,71 +1,189 @@
 #include "veml7700.h"
+#include "esphome/core/helpers.h"
 #include "esphome/core/log.h"
 
 namespace esphome {
 namespace veml7700 {
 
-static const char *const TAG = "VEML7700";
-
-static const uint8_t TSL2561_COMMAND_BIT = 0x80;
-// config
-static const uint8_t CONFIGURATION_REGISTER    = 0x00;
-static const uint16_t ALS_POWEROFF             = 0x1;
-static const uint16_t ALS_POWERON              = 0x0;
-static const uint16_t INTERRUPT_DISABLE        = 0x0;
-static const uint16_t ALS_PERS_1               = 0x0;
-static const uint16_t ALS_PERS_2               = 0x10;
-static const uint16_t ALS_PERS_4               = 0x20;
-static const uint16_t ALS_PERS_8               = 0x30;
-
-//power save
-const uint8_t POWER_SAVING_REGISTER = 0x03;
-const uint8_t PSM_1                 = 0x0;
-const uint8_t PSM_2                 = 0x02;
-const uint8_t PSM_3                 = 0x04;
-const uint8_t PSM_4                 = 0x06;
-const uint8_t PSM_EN                = 0x01;
-const uint8_t PSM_DIS               = 0x0;
-
-// measurements
-const uint8_t ALS_REGISTER          = 0x04;
-const uint8_t WHITE_REGISTER        = 0x05;
+static const char *const TAG = "veml7700.sensor";
 
 void VEML7700Sensor::setup() {
-  ESP_LOGCONFIG(TAG, "Setting up VEML7700...");
+  uint8_t device_id[] = {0, 0};
 
-  uint16_t integration_time = this->integration_time_;
-  uint16_t gain = this->gain_;
-  uint16_t psm = this->psm_;
-  uint16_t poweron = ALS_POWERON;
-  uint16_t setting = poweron | integration_time | gain;
-    
-  if (!this->veml7700_write_uint(CONFIGURATION_REGISTER, setting)) 
-  {
-     this->mark_failed();
-     return;   
+  ESP_LOGCONFIG(TAG, "Setting up VEML7700 '%s'...",, this->name_.c_str());
+  if (!this->refresh_config_reg()) {
+    ESP_LOGE(TAG, "Unable to write configuration");
+    this->mark_failed();
+    return;
   }
-  else
-  {
-    ESP_LOGCONFIG(TAG, "Power on, integration time, and gain are set %u", ALS_POWERON | integration_time | gain);
+  if ((this->write(&ID_REG, 1, false) != i2c::ERROR_OK) || !this->read_bytes_raw(device_id, 2)) {
+    ESP_LOGE(TAG, "Unable to read ID");
+    this->mark_failed();
+    return;
+  } else if (device_id[0] != DEVICE_ID) {
+    ESP_LOGE(TAG, "Incorrect device ID - expected 0x%.2x, read 0x%.2x", DEVICE_ID, device_id[0]);
+    this->mark_failed();
+    return;
   }
-
-  auto timeout = static_cast<uint32_t>(this->get_integration_time_ms_() + 20);
-  this->set_timeout("illuminance", timeout, [this]() { this->read_data_(); });
-  
-  uint16_t psm_en = PSM_EN;
-  uint16_t setting_psm = psm_en | psm;
-  
-  if (!this->veml7700_write_uint(POWER_SAVING_REGISTER, setting_psm))
-  {
-     this->mark_failed();
-     return;   
-  }
-  else
-  {
-    ESP_LOGCONFIG(TAG, "Power save registers are set %u", PSM_EN | psm);
-  }
-  auto timeout = static_cast<uint32_t>(20);
 }
+
+bool VEML7700Sensor::refresh_config_reg(bool force_on) {
+  uint16_t data = this->power_on_ || force_on ? ALS_POWERON : ALS_POWEROFF;
+
+  data |= (uint16_t(this->integration_time_));
+  data |= (uint16_t(this->gain_));
+
+  ESP_LOGVV(TAG, "Writing 0x%.4x to register 0x%.2x", data, CONFIG_REG);
+  bool a_return = this->write_byte_16(CONFIG_REG, data);
+  
+  uint16_t data = PSM_EN;
+  data |= (uint16_t(this->psm_));
+  uint16_t setting_psm = psm_en | psm;
+  ESP_LOGVV(TAG, "Writing 0x%.4x to register 0x%.2x", data, POWER_SAVING_REGISTER);
+  bool b_return = this->write_byte_16(POWER_SAVING_REGISTER, data);
+  
+  return a_return & b_return;
+}
+
+float VEML7700Sensor::read_lx_() {
+  if (!this->power_on_) {  // if off, turn on
+    if (!this->refresh_config_reg(true)) {
+      ESP_LOGW(TAG, "Turning on failed");
+      this->status_set_warning();
+      return NAN;
+    }
+    delay(4);  // from RM: a wait time of 4 ms should be observed before the first measurement is picked up, to allow
+               // for a correct start of the signal processor and oscillator
+  }
+
+  uint8_t als_regs[] = {0, 0};
+  if ((this->write(&ALS_REGISTER, 1, false) != i2c::ERROR_OK) || !this->read_bytes_raw(als_regs, 2)) {
+    this->status_set_warning();
+    return NAN;
+  }
+
+  this->status_clear_warning();
+
+  float als_raw_value_multiplier = LUX_MULTIPLIER_BASE;
+  uint16_t als_raw_value = encode_uint16(als_regs[1], als_regs[0]);
+  // determine multiplier value based on gains and integration time
+  switch (this->gain_) {
+    case VEML7700_GAIN_0p25X:
+      als_raw_value_multiplier *= 8;
+      break;
+    case VEML7700_GAIN_0p125X:
+      als_raw_value_multiplier *= 16;
+      break;
+    case VEML7700_GAIN_1X:
+      als_raw_value_multiplier *= 2;
+      break;
+    case VEML7700_GAIN_2X:
+      als_raw_value_multiplier *=1;
+      break;
+    default:
+      break;
+  }
+  
+  switch (this->integration_time_)
+  {
+    case VEML7700_INTEGRATION_200MS:
+      als_raw_value_multiplier *= 0.25;
+      break;
+    case VEML7700_INTEGRATION_400MS:
+      als_raw_value_multiplier *= 0.5;
+      break;
+    case VEML7700_INTEGRATION_800MS:
+      als_raw_value_multiplier *= 1;
+      break;
+    case VEML7700_INTEGRATION_50MS:
+      als_raw_value_multiplier *= 0.625;
+      break;
+    case VEML7700_INTEGRATION_25MS:
+      als_raw_value_multiplier *= 0.03125;
+      break;
+    default:
+      break;
+  } 
+  // finally, determine and return the actual lux value
+  float lx = float(als_raw_value) * als_raw_value_multiplier;
+  ESP_LOGVV(TAG, "'%s': ALS raw = %u, multiplier = %.5f", this->get_name().c_str(), als_raw_value,
+            als_raw_value_multiplier);
+  ESP_LOGD(TAG, "'%s': Illuminance = %.4flx", this->get_name().c_str(), lx);
+
+  if (!this->power_on_) {  // turn off if required
+    if (!this->refresh_config_reg()) {
+      ESP_LOGW(TAG, "Turning off failed");
+      this->status_set_warning();
+    }
+  }
+
+  if (this->auto_gain_) {
+    this->adjust_gain_(als_raw_value);
+  }
+
+  return lx;
+}
+
+void VEML7700Sensor::adjust_gain_(const uint16_t als_raw_value) {
+  if ((als_raw_value > UINT16_MAX * this->auto_gain_threshold_low_) &&
+      (als_raw_value < UINT16_MAX * this->auto_gain_threshold_high_)) {
+    return;
+  }
+
+  if (als_raw_value >= UINT16_MAX * 0.9) {  // over-saturated, reset all gains and start over
+    this->digital_gain_ = VEML7700_DIGITAL_GAIN_1X;
+    this->gain_ = VEML7700_GAIN_1X;
+    this->integration_time_ = VEML7700_INTEGRATION_TIME_50MS;
+    this->refresh_config_reg();
+    return;
+  }
+
+  if (this->gain_ != VEML7700_GAIN_2X) {  // increase gain if possible
+    switch (this->gain_) {
+      case VEML7700_GAIN_0p125X:
+        this->gain_ = VEML7700_GAIN_0p25X;
+        break;
+      case VEML7700_GAIN_0p25X:
+        this->gain_ = VEML7700_GAIN_1X;
+        break;
+      case VEML7700_GAIN_1X:
+        this->gain_ = VEML7700_GAIN_2X;
+        break;
+      default:
+        break;
+    }
+    this->refresh_config_reg();
+    return;
+  }
+
+  // digital gain is maxed out; reset it and try to increase integration time
+  if (this->integration_time_ != VEML7700_INTEGRATION_800MS) {  // increase integration time if possible
+    switch (this->integration_time_) {
+      case VEML7700_INTEGRATION_25MS:
+        this->integration_time_ = VEML7700_INTEGRATION_50MS;
+        break;
+      case VEML7700_INTEGRATION_50MS:
+        this->integration_time_ = VEML7700_INTEGRATION_100MS;
+        break;
+      case VEML7700_INTEGRATION_100MS:
+        this->integration_time_ = VEML7700_INTEGRATION_200MS;
+        break;
+      case VEML7700_INTEGRATION_200MS:
+        this->integration_time_ = VEML7700_INTEGRATION_400MS;
+        break;
+      case VEML7700_INTEGRATION_400MS:
+        this->integration_time_ = VEML7700_INTEGRATION_400MS;
+        break;
+      default:
+        break;
+    }
+    this->gain_ = VEML7700_GAIN_0p125X;
+    this->refresh_config_reg();
+    return;
+  }
+}
+
+
   
 void VEML7700Sensor::dump_config() {
   LOG_SENSOR("", "VEML7700", this);
@@ -93,130 +211,5 @@ void VEML7700Sensor::dump_config() {
   LOG_UPDATE_INTERVAL(this);
 }
 
-void VEML7700Sensor::update() {
-  this->read_data_();
-}
-
-float VEML7700Sensor::calculate_lx_(uint16_t als) {
-  float als_f = als;
-  float magic_number = 0.0288;
-  switch (this->integration_time_)
-  {
-    case VEML7700_INTEGRATION_200MS:
-      magic_number = 0.0144;
-    case VEML7700_INTEGRATION_400MS:
-      magic_number = 0.0072;
-    case VEML7700_INTEGRATION_800MS:
-      magic_number = 0.0036;
-    case VEML7700_INTEGRATION_50MS:
-      magic_number = 0.0576;
-    case VEML7700_INTEGRATION_25MS:
-      magic_number = 0.1152;
-    default:
-      magic_number = 0.0288;
-  } 
-  float a_gain = 1;
-  switch (this->gain_)
-  {
-    case VEML7700_GAIN_1X :
-      a_gain = 0.5;
-    case VEML7700_GAIN_0p25X :
-      a_gain = 8;
-    case VEML7700_GAIN_0p125X :
-      a_gain = 16;
-    default:
-      a_gain = 1;
-  }
-  float lx = als_f * magic_number * a_gain;
-  return lx;
-}
-
-void VEML7700Sensor::read_data_() {
-  uint16_t als;
-  if (!this->veml7700_read_uint(ALS_REGISTER, &als)) {
-    this->status_set_warning();
-    return;
-  }
-  uint16_t white;
-  if (!this->veml7700_read_uint(WHITE_REGISTER, &white)) {
-    this->status_set_warning();
-    return;
-  }
-  if (als == 0) {
-    if (!this->read_byte_16(ALS_REGISTER, &als)) {
-      this->status_set_warning();
-      return;
-    }
-  }
-  ESP_LOGD(TAG, "Got illuminance=%u ", als);
-  ESP_LOGD(TAG, "Got illuminance white =%u ", white);
-  float lx = this->calculate_lx_(als);
-  ESP_LOGD(TAG, "Got illuminance=%.1flx", lx);
-  this->publish_state(lx);
-  this->status_clear_warning();
-}
-  
-float VEML7700Sensor::get_integration_time_ms_() {
-  switch (this->integration_time_) {
-    case VEML7700_INTEGRATION_200MS:
-      return 200.0f;
-    case VEML7700_INTEGRATION_400MS:
-      return 400.0f;
-    case VEML7700_INTEGRATION_800MS:
-      return 800.0f;
-    case VEML7700_INTEGRATION_50MS:
-      return 50.0f;
-    case VEML7700_INTEGRATION_25MS:
-      return 25.0f;
-    default:
-      return 100.0f;
-  }
-  return 0.0f;
-}
-  
-float VEML7700Sensor::get_gain_() {
-  switch (this->gain_) {
-    case VEML7700_GAIN_1X :
-      return 1.0f;
-    case VEML7700_GAIN_0p25X :
-      return 0.25f;
-    case VEML7700_GAIN_0p125X :
-      return 0.125f;
-    default:
-      return 2.0f;
-  }
-  return 0.0f;
-}
-  
-void VEML7700Sensor::set_integration_time(VEML7700IntegrationTime integration_time) {
-  this->integration_time_ = integration_time;
-}
-void VEML7700Sensor::set_gain(VEML7700Gain gain) { this->gain_ = gain; }
-void VEML7700Sensor::set_psm(VEML7700PSM psm) { this->psm_ = psm; }
-  
-float VEML7700Sensor::get_setup_priority() const { return setup_priority::DATA; }
-
-bool VEML7700Sensor::veml7700_read_uint(uint8_t a_register, uint16_t *value) {
-  uint8_t data[2];
-  if (!this->read_bytes(a_register, data, 2))
-    return false;
-  const uint16_t hi = data[1];
-  const uint16_t lo = data[0];
-  *value = (hi << 8) | lo;
-  return true;
-}
-
-//bool VEML7700Sensor::veml7700_read_bytes_16(uint8_t a_register, uint16_t *value) {
-//  uint16_t data;
-//  if (!this->read_bytes_16(a_register, &data, 1))
-//    return false;
-//  *value = data;
-//  return true;
-//}
-
-bool VEML7700Sensor::veml7700_write_uint(uint8_t a_register, uint16_t value) {
-  return this->write_byte_16(a_register, value);
-}
-  
 }  // namespace tsl2561
 }  // namespace esphome
