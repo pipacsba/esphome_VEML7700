ada1b8abe8b1474fde08afcc5f974a440adfa39d
pipacsba <44190358+pipacsba@users.noreply.github.com>
2024-02-23 20:05:11 +0100

    Update veml7700.cpp

--- components/veml7700/veml7700.cpp
+++ components/veml7700/veml7700.cpp
@@ -37,7 +37,7 @@ bool VEML7700Sensor::refresh_config_reg(bool force_on)
   uint16_t data = this->power_on_ || force_on ? ALS_POWERON : ALS_POWEROFF;
   data |= (uint16_t(this->integration_time_));
   data |= (uint16_t(this->gain_));
-  ESP_LOGD(TAG, "Writing 0x%.4x to register 0x%.2x", data, CONFIGURATION_REGISTER);
+  ESP_LOGD(TAG, "Turn on ALS and set up configuration: Writing 0x%.4x to register 0x%.2x", data, CONFIGURATION_REGISTER);
   bool b_return = this->write_byte_16(CONFIGURATION_REGISTER, data);
 
   data = PSM_EN;
